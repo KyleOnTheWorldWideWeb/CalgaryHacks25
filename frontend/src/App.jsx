ac44c5f0ab20a87102380128271003b69fccb684
Pranav Kumar <pranav.pk.kumar2003@gmail.com>
2025-02-15 15:42:39 -0700

    Added some static styles
    Added Header and Color to the page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,11 @@
-import { useState } from "react";
 import { Route, Routes } from "react-router-dom";
-import Home from "./Pages/Home.jsx";
+import Home from "./Pages/Home";
 
 function App() {
   return (
     <Routes>
-      <Route path="WildChanging" element={<Home />} />
-      <Route path="/" element={<h1>Hello</h1>} />
+      <Route path="/" element={<Home />} />
+      <Route path="hello" element={<h1>Hello</h1>} />
     </Routes>
   );
 }
--- frontend/src/Pages/Home.jsx
+++ frontend/src/Pages/Home.jsx
@@ -1,17 +1,18 @@
-import NavBar from '../Components/NavBar.jsx';
-import MainContainer from '../Components/MainContainer.jsx';
-import React, { useEffect, useState } from 'react';
-import '../Styles/root_local.scss';
-import MapInteraction from "../Components/MapInteraction";
+import NavBar from "../Componets/NavBar.jsx";
+import MainContainer from "../Componets/MainContainer.jsx";
+import React, { useEffect, useState } from "react";
+import "../Styles/root_local.scss";
+import MapInteraction from "../Componets/MapInteraction.jsx";
+import Header from "../Componets/Header.jsx";
 
-function Root () {
+function Root() {
   return (
-    <>
-      <NavBar></NavBar>
+    <div className="bg-gray-900">
+      <Header />
       <MainContainer>
         <MapInteraction />
       </MainContainer>
-    </>
+    </div>
   );
 }
 
