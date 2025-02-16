da7c2e72f910c092ad41cb2e5407cc07243e286f
Kyle W. <8kwest@gmail.com>
2025-02-16 02:03:15 -0700

    some style changes.

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,12 +1,13 @@
 export default function Example() {
   return (
-    <div className="relative isolate overflow-hidden bg-gray-900 py-12 sm:py-16">
-      <div className="mx-auto max-w-7xl px-6 lg:px-8">
-        <div className="mx-auto max-w-2xl lg:mx-0">
-          <h3 className="text-5xl font-semibold tracking-tight text-white sm:text-7xl">
-            Wild-Changing
+    <div className="relative isolate overflow-hidden bg-gray-900 py-8 sm:py-8">
+      <div className="max-w-7xl px-8 lg:px-16">
+        <div className="max-w-1xl lg:mx-0 text-left">
+          <h3 className="text-5xl font-semibold tracking-tight text-white sm:text-7xl underline-offset-2 underline">
+            Wild-Change
           </h3>
         </div>
+        <span className="px-8 text-2xl text-yellow-50">Wildlife & Climate Change Visualizer.</span>
       </div>
     </div>
   );
--- frontend/src/components/TopRowContainer.jsx
+++ frontend/src/components/TopRowContainer.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import '../Styles/top_row_container.scss';
 
 const TopRowContainer = ({ children, id }) => {
-  return <div className="top-row-container" id={id}>{children}</div>;
+  return <div className="top-row-container rounded-lg" id={id}>{children}</div>;
 };
 
 export default TopRowContainer;
