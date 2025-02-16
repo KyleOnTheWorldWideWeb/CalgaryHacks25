e6259987be6a7e28cef910fc61445bc5a2d3911a
Kyle W. <8kwest@gmail.com>
2025-02-16 02:10:03 -0700

    styling out

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,6 +1,6 @@
 export default function Example() {
   return (
-    <div className="relative isolate overflow-hidden bg-gray-900 py-8 sm:py-8">
+    <div className="relative isolate overflow-hidden bg-gray-900 py-6 sm:py-6">
       <div className="max-w-7xl px-8 lg:px-16">
         <div className="max-w-1xl lg:mx-0 text-left">
           <h3 className="text-5xl font-semibold tracking-tight text-white sm:text-7xl underline-offset-2 underline">
